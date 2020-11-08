7be3d152d4b5495b270becd9956ff479fb84d636
Oyewole Oni <ooni@eminenttechnology.com>
2020-11-08 19:35:13 +0100

    Connecting to MongoDB and implementation of socket.io

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -14,16 +14,23 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+var dburl = 'mongodb+srv://user:tQLVD3ziqYJGCdB@cluster0.upvla.mongodb.net/chatApp'
+
 app.get('/messages', (req, res) => {
     res.send(messages);
 })
 app.post('/messages', (req, res) => {
     console.log(req.body);
     messages.push(req.body);
+    io.emit('message', req.body);
     res.sendStatus(200);
 })
 io.on('connection', (socket) =>{
     console.log('a user connected');
+})
+
+mongoose.connect(dburl, { useUnifiedTopology: true , useNewUrlParser: true},(err) =>{
+    console.log('mongo db connection', err)
 })
 var server = http.listen(3000, ()=> {
     console.log('server is listening on port', server.address().port)
